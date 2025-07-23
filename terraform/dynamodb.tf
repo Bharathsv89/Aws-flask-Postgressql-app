b174186994e73d0e1d56b90ed08378ece1877e23
Bharathsv89 <bharathsv89@gmail.com>
2025-07-23 12:59:22 +0530

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-locks"
+  name           = "terraform-locks001"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
   attribute {
